d43a2036d0634d7b1cbc064dd01a9218b13ade00
jamesdeklerk <jamesdeklerk@gmail.com>
2018-05-28 12:53:28 +0200

    Adjusted window positions for Ubuntu & fixed exiting windows

--- image_processing_tests/open_cv_image_processing.py
+++ image_processing_tests/open_cv_image_processing.py
@@ -18,6 +18,8 @@ scaled_width = 9
 scaled_height = 9
 
 window_size = 300
+window_size_extra_width = 100
+window_size_extra_height = 80
 
 # Load an color image in grayscale
 original = cv2.imread('depth_images/dont_use_in_masters.jpg',0)
@@ -30,39 +32,40 @@ INTER_LANCZOS4 = cv2.resize(original, (scaled_width, scaled_height), interpolati
 
 cv2.namedWindow('original', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('original', window_size, window_size)
-cv2.moveWindow('original', int(window_size * 0), int((window_size * 0.5) + (31 * 0.5)))
+cv2.moveWindow('original', int(window_size * 0), int((window_size * 0.5) + (window_size_extra_height * 0.5)))
 cv2.imshow('original',original)
 
 cv2.namedWindow('small_default', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('small_default', window_size, window_size)
-cv2.moveWindow('small_default', window_size * 1, 0)
+cv2.moveWindow('small_default', (window_size * 1) + window_size_extra_width, 0)
 cv2.imshow('small_default',small_default)
 
 cv2.namedWindow('INTER_NEAREST', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('INTER_NEAREST', window_size, window_size)
-cv2.moveWindow('INTER_NEAREST', window_size * 2, 0)
+cv2.moveWindow('INTER_NEAREST', (window_size * 2) + window_size_extra_width, 0)
 cv2.imshow('INTER_NEAREST',INTER_NEAREST)
 
 cv2.namedWindow('INTER_LINEAR', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('INTER_LINEAR', window_size, window_size)
-cv2.moveWindow('INTER_LINEAR', window_size * 3, 0)
+cv2.moveWindow('INTER_LINEAR', (window_size * 3) + window_size_extra_width, 0)
 cv2.imshow('INTER_LINEAR',INTER_LINEAR)
 
 cv2.namedWindow('INTER_AREA', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('INTER_AREA', window_size, window_size)
-cv2.moveWindow('INTER_AREA', window_size * 1, (window_size * 1) + 31)
+cv2.moveWindow('INTER_AREA', (window_size * 1) + window_size_extra_width, (window_size * 1) + window_size_extra_height)
 cv2.imshow('INTER_AREA',INTER_AREA)
 
 cv2.namedWindow('INTER_CUBIC', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('INTER_CUBIC', window_size, window_size)
-cv2.moveWindow('INTER_CUBIC', window_size * 2, (window_size * 1) + 31)
+cv2.moveWindow('INTER_CUBIC', (window_size * 2) + window_size_extra_width, (window_size * 1) + window_size_extra_height)
 cv2.imshow('INTER_CUBIC',INTER_CUBIC)
 
 cv2.namedWindow('INTER_LANCZOS4', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('INTER_LANCZOS4', window_size, window_size)
-cv2.moveWindow('INTER_LANCZOS4', window_size * 3, (window_size * 1) + 31)
+cv2.moveWindow('INTER_LANCZOS4', (window_size * 3) + window_size_extra_width, (window_size * 1) + window_size_extra_height)
 cv2.imshow('INTER_LANCZOS4',INTER_LANCZOS4)
 
+# click Esc key (with one of the image windows in focus) to stop
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+exit()
