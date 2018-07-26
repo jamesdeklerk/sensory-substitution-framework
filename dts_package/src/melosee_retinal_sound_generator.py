39a9ac2c9b00f60d78fd78479b8fbfe17014846d
jamesdeklerk <jamesdeklerk@gmail.com>
2018-07-26 14:54:25 +0200

    Added hard coded frequencies

--- dts_package/src/melosee_retinal_sound_generator.py
+++ dts_package/src/melosee_retinal_sound_generator.py
@@ -3,6 +3,16 @@
 import math
 import numpy as np
 
+# C-major, the scale of just intonation would be: C=1/1 D=9/8 E=5/4 F= 4/3 G=3/2 A=5/3 B=15/8 C=2/1
+C_4 = 264.0
+D = C_4 * (9.0/8.0)
+E = C_4 * (5.0/4.0)
+F = C_4 * (4.0/3.0)
+G = C_4 * (3.0/2.0)
+A = C_4 * (5.0/3.0)
+B = C_4 * (15.0/8.0)
+C_5 = C_4 * (2.0/1.0)
+
 # normalize value in [min_output, max_output]
 def normalize(value, original_min, original_max, min_output, max_output):
 
