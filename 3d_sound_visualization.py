5c72fbc7b7ac400a398e1a562791fa59086d9483
James de Klerk <jamesdeklerk@gmail.com>
2018-08-05 16:34:14 +0200

    Added multiple speakers

--- 3d_sound_visualization.py
+++ 3d_sound_visualization.py
@@ -11,16 +11,12 @@ import numpy as np
 import time
 import sys
 
-def get_position(GLMeshItem):
-    transform = GLMeshItem.transform()
-    x = transform.row(0).w()
-    y = transform.row(1).w()
-    z = transform.row(2).w()
-    return (x, y, z)
-
 class Window(QtGui.QWidget):
+
     def __init__(self):
         QtGui.QWidget.__init__(self)
+
+        self.speakers = []
 
         self.view_widget = gl.GLViewWidget(self)
         self.view_widget.setCameraPosition(distance=50, elevation=30, azimuth=0)
@@ -29,10 +25,30 @@ class Window(QtGui.QWidget):
         layout = QtGui.QVBoxLayout(self)
         layout.addWidget(self.view_widget)
 
+        self.setup_grid()
+
+        head_mesh_data = gl.MeshData.sphere(rows=10, cols=10)
+        head = gl.GLMeshItem(meshdata=head_mesh_data, smooth=True, shader='shaded', glOptions='opaque')
+        head.translate(0, 0, 0)
+        self.view_widget.addItem(head)
+
+        num_x = 10
+        num_y = 10
+        count = 0
+        for x in xrange(num_x):
+            for y in xrange(num_y):
+                self.speakers.append(self.add_speaker(x, y, 5, 0.5))
+                count = count + 1
+
+    def start(self):
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
+
+    def setup_grid(self):
         # NOTE: The axis are according to the right hand rule
         # +z is up
         # +y is right
-        # +x is towards you (from the screen)f
+        # +x is towards you (from the screen)
 
         x_grid = gl.GLGridItem()
         x_grid.scale(1,1,1)
@@ -47,25 +63,33 @@ class Window(QtGui.QWidget):
         z_grid.scale(1,1,1)
         z_grid.rotate(90, 1, 0, 0)
         self.view_widget.addItem(z_grid)
+    
+    def get_position(self, GLMeshItem):
+        transform = GLMeshItem.transform()
+        x = transform.row(0).w()
+        y = transform.row(1).w()
+        z = transform.row(2).w()
+        return (x, y, z)
 
-        head_mesh_data = gl.MeshData.sphere(rows=10, cols=10)
-        head = gl.GLMeshItem(meshdata=head_mesh_data, smooth=True, shader='shaded', glOptions='opaque')
-        head.translate(0, 0, 0)
-        self.view_widget.addItem(head)
+    def transform_speaker(self, speaker, x, y, z, size):
+        """
+        Move and scale ball
+        """
+        speaker.resetTransform()
+        speaker.translate(x, y, z)
+        speaker.scale(size, size, size)
 
+    def add_speaker(self, x, y, z, size):
         speaker_mesh_data = gl.MeshData.sphere(rows=10, cols=10)
-        self.speaker = gl.GLMeshItem(meshdata=speaker_mesh_data, smooth=True, shader='viewNormalColor', glOptions='opaque')
-        self.speaker.translate(5, 0, 5)
-        self.speaker.scale(0.5, 0.5, 0.5)
-        self.view_widget.addItem(self.speaker)
+        speaker = gl.GLMeshItem(meshdata=speaker_mesh_data, smooth=True, shader='viewNormalColor', glOptions='opaque')
+        self.transform_speaker(speaker, x, y, z, size)
+        self.view_widget.addItem(speaker)
+        return speaker
 
-    def start(self):
-        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-            QtGui.QApplication.instance().exec_()
-
-    def update(self, x, y, z):
-        self.speaker.translate(x, y, z)
-        print get_position(self.speaker)
+    def update(self, speaker, x, y, z):
+        position = self.get_position(speaker)
+        self.transform_speaker(speaker, position[0] + x, position[1] + y, position[2] + z, 1.5)
+        print self.get_position(speaker)
 
     def animate(self):
         # timer = QtCore.QTimer()
@@ -82,22 +106,22 @@ class Window(QtGui.QWidget):
 
             # move up
             if key == QtCore.Qt.Key_W:
-                self.update(0, 0, MOVE_DISTANCE)
+                self.update(self.speakers[0], 0, 0, MOVE_DISTANCE)
             # move down
             if key == QtCore.Qt.Key_S:
-                self.update(0, 0, -MOVE_DISTANCE)
+                self.update(self.speakers[0], 0, 0, -MOVE_DISTANCE)
             # move left
             if key == QtCore.Qt.Key_A:
-                self.update(0, -MOVE_DISTANCE, 0)
+                self.update(self.speakers[0], 0, -MOVE_DISTANCE, 0)
             # move right
             if key == QtCore.Qt.Key_D:
-                self.update(0, MOVE_DISTANCE, 0)
+                self.update(self.speakers[0], 0, MOVE_DISTANCE, 0)
             # move backwards
             if key == QtCore.Qt.Key_F:
-                self.update(MOVE_DISTANCE, 0, 0)
+                self.update(self.speakers[0], MOVE_DISTANCE, 0, 0)
             # move forwards (into screen)
             if key == QtCore.Qt.Key_R:
-                self.update(-MOVE_DISTANCE, 0, 0)
+                self.update(self.speakers[0], -MOVE_DISTANCE, 0, 0)
             
             
         return QtGui.QWidget.eventFilter(self, widget, event)
